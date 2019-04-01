7186cafc02a8aba28993db86d0069988af65542b
dietic <dierios93@gmail.com>
2019-04-01 17:39:04 -0500

    Testeo arreglo de scripts

--- includes/_custom-scripts.php
+++ includes/_custom-scripts.php
@@ -11,6 +11,6 @@
 
 
 
-<script type="text/javascript" src="../js/plugins/sidenav.js"></script>
-<script type="text/javascript" src="../js/plugins/smooth-scroll.js"></script>
-<script type="text/javascript" src="../js/plugins/extras.js"></script>
+<script type="text/javascript" src="js/plugins/sidenav.js"></script>
+<script type="text/javascript" src="js/plugins/smooth-scroll.js"></script>
+<script type="text/javascript" src="js/plugins/extras.js"></script>
--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
         <div class="row">
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <div class="col l2 logo">
-            <a href="#!" class="right brand-logo">BAL<span>ANS</span><span class="vertical-divider">|</span></a>
+            <a href="#home" class="right brand-logo">BAL<span>ANS</span><span class="vertical-divider">|</span></a>
           </div>
           <div class="col s10">
             <div class="col s6 offset-s3" >
@@ -21,9 +21,9 @@
     </nav>
 
     <ul class="sidenav" id="mobile-demo">
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Sobre mi</a></li>
-      <li><a href="#">Features</a></li>
+      <li><a href="#home">Inicio</a></li>
+      <li><a href="#about">Nosotros</a></li>
+      <li><a href="#">Servicios</a></li>
       <li><a href="#">Contacto</a></li>
     </ul>
   </div>
